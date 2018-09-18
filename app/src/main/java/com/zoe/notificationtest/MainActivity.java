2b62bbf248f2db3d444f4d5bf2b11ba1f384d2c7
ZoeWangW <13438165026@163.com>
2018-09-18 22:45:39 +0800

    SetLatestEventInfo已经被弃用，新方法试了还是不行，需要继续琢磨
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/notificationtest/MainActivity.java
+++ app/src/main/java/com/zoe/notificationtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.zoe.notificationtest;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
