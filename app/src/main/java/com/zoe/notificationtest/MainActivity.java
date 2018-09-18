7d8121fc4a096497690b4e7dd9eaedaa1975d98b
ZoeWangW <13438165026@163.com>
2018-09-18 22:13:45 +0800

    新建项目测试通知适用方法（失败，未找出问题）
    Signed-off-by: ZoeWangW <13438165026@163.com>

---
+++ app/src/main/java/com/zoe/notificationtest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.zoe.notificationtest;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private Button sendNotice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       sendNotice = (Button) findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case  R.id.send_notice:
+                        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                        Notification notification = new Notification(R.mipmap.ic_launcher,"This is ticker text",System.currentTimeMillis());
+                        notification.setLatestEventInfo(this, "This is content title", "This is content text", null);
+                        manager.notify(1,notification);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+}
