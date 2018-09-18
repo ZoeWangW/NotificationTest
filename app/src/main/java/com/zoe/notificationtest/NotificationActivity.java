1fde8368d1a2e83115a36f4193af9ecbe12c3185
ZoeWangW <13438165026@163.com>
2018-09-18 22:59:52 +0800

    取消状态栏显示，调用close（）
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/notificationtest/NotificationActivity.java
+++ app/src/main/java/com/zoe/notificationtest/NotificationActivity.java
@@ -1,6 +1,7 @@
 package com.zoe.notificationtest;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.os.Bundle;
 
 public class NotificationActivity extends Activity {
@@ -8,5 +9,7 @@ public class NotificationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_layout);
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        manager.cancel(1);
     }
 }
